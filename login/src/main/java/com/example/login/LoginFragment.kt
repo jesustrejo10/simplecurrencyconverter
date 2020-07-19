d77a16201c4b3d250e87555b49eebba22d326f99
Jesus <jesustrejo10@gmail.com>
2020-07-19 11:41:55 -0400

    Login ui, with basic validations.

--- login/src/main/java/com/example/login/LoginFragment.kt
+++ login/src/main/java/com/example/login/LoginFragment.kt
@@ -1,14 +1,17 @@
 package com.example.login
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.login.LoginViewModel.UIValidator.*
+import kotlinx.android.synthetic.main.login_fragment.*
 
 
-class LoginFragment : Fragment() {
+class LoginFragment : Fragment(), View.OnClickListener {
 
     private lateinit var viewModel: LoginViewModel
 
@@ -21,8 +24,45 @@ class LoginFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        login_action.setOnClickListener(this)
+        viewModel.operationResultManager.observe(viewLifecycleOwner, Observer {
+            manageOperationResult(it)
+        })
+
+        viewModel.uiStatusManager.observe(viewLifecycleOwner, Observer {
+            manageUIStatus(it)
+        })
     }
 
+    private fun manageUIStatus(uiStatus: LoginViewModel.UIValidator?) {
+        uiStatus ?: return
+        when(uiStatus){
+            INVALID_EMAIL ->{
+                email.requestFocus()
+                email.error = getString(R.string.wrong_email_address)
+            }
+            INVALID_PASSWORD -> {
+                password.requestFocus()
+                password.error = getString(R.string.empty_password)
+            }
+            INVALID_EMAIL_LENGHT -> {
+                password.requestFocus()
+                password.error = getString(R.string.wrong_password)
+            }
+        }
+    }
+
+    override fun onClick(view: View?) {
+        if(view != null){
+            if(view.id == R.id.login_action){
+                viewModel.verifyScreen(email.text.toString(),password.text.toString())
+            }
+        }
+    }
+
+    private fun manageOperationResult(operationResult: LoginViewModel.OperationResult?) {
+
+
+    }
 }
--- login/src/main/java/com/example/login/LoginViewModel.kt
+++ login/src/main/java/com/example/login/LoginViewModel.kt
@@ -1,7 +1,39 @@
 package com.example.login
 
+import android.text.TextUtils
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val uiStatusManager = MutableLiveData<UIValidator>()
+    val operationResultManager = MutableLiveData<OperationResult>()
+
+    fun verifyScreen(email: String, password: String) {
+        val validEmail =  !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+        if(!validEmail) {
+            uiStatusManager.postValue(UIValidator.INVALID_EMAIL)
+            return
+        }
+        var validPassword = password.isNotEmpty()
+        if(!validPassword)
+            uiStatusManager.postValue(UIValidator.INVALID_PASSWORD)
+        else{
+            validPassword = password.length > 4
+            if(!validPassword)
+                uiStatusManager.postValue(UIValidator.INVALID_EMAIL_LENGHT)
+        }
+        if(validEmail && validPassword){
+            operationResultManager.postValue(OperationResult.LOGIN_REQUESTED)
+        }
+
+    }
+
+    enum class UIValidator {
+        INVALID_EMAIL, INVALID_PASSWORD,INVALID_EMAIL_LENGHT
+    }
+
+    enum class OperationResult {
+        LOGIN_REQUESTED
+    }
 }
