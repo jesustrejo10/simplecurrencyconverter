a777e73065f14068c7a156b299574ac564cd9e3e
Jesus <jesustrejo10@gmail.com>
2020-07-19 15:09:56 -0400

    Creating logic to read the values from UI

--- converter/src/main/java/com/example/converter/ConversorFragment.kt
+++ converter/src/main/java/com/example/converter/ConversorFragment.kt
@@ -6,12 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.conversor_fragment.*
 
 
 class ConversorFragment : Fragment(), AdapterView.OnItemClickListener,
-    AdapterView.OnItemSelectedListener {
+    AdapterView.OnItemSelectedListener, View.OnClickListener {
 
     private lateinit var viewModel: ConversorViewModel
 
@@ -33,6 +34,30 @@ class ConversorFragment : Fragment(), AdapterView.OnItemClickListener,
     private fun manageViewComponents() {
 
         currencySpinner.onItemSelectedListener= this
+        convert_action.setOnClickListener(this)
+
+        viewModel.uiStateManager.observe(viewLifecycleOwner, Observer {
+            manageUIStatus(it)
+        })
+    }
+
+    private fun manageUIStatus(uiState: ConversorViewModel.UIState?) {
+        uiState?: return
+        when(uiState.uiState){
+            ConversorViewModel.UIStateOption.EMPTY -> {
+                currencyValue.requestFocus()
+                currencyValue.error = getString(R.string.empty_status)
+            }
+            ConversorViewModel.UIStateOption.WRONG_VALUE -> {
+
+                currencyValue.requestFocus()
+                currencyValue.error = getString(R.string.wrong_value)
+            }
+            ConversorViewModel.UIStateOption.CORRECT_VALUE ->{
+                println("")
+                //requestVal
+            }
+        }
     }
 
     override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
@@ -43,6 +68,13 @@ class ConversorFragment : Fragment(), AdapterView.OnItemClickListener,
 
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
         viewModel.manageSelectedCurrency(position)
+    }
+
+    override fun onClick(v: View?) {
+        v?: return
+        if(v.id == R.id.convert_action){
+            viewModel.verifyUi(currencyValue.text.toString())
+        }
     }
 
 }
--- converter/src/main/java/com/example/converter/ConversorViewModel.kt
+++ converter/src/main/java/com/example/converter/ConversorViewModel.kt
@@ -1,26 +1,53 @@
 package com.example.converter
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ConversorViewModel : ViewModel() {
 
+    var currentCurrency = Currency.USD
+    var currentFloatVal = 0f
+    var uiStateManager = MutableLiveData<UIState>()
 
     fun manageSelectedCurrency(position: Int) {
 
-        println("")
+        currentCurrency = getCurrencyByPosition(position)
     }
 
-    private fun getCurrencyByPosition(position : Int){
+    private fun getCurrencyByPosition(position : Int): Currency {
 
-        when (position){
+        return when (position){
             0 -> Currency.USD
             1 -> Currency.EUR
             2 -> Currency.BS
             3 -> Currency.BTC
             4 -> Currency.ETH
             5 -> Currency.PTR
+            else -> Currency.USD
         }
     }
+
+    fun verifyUi(toString: String) {
+        if(toString.isEmpty())
+            uiStateManager.postValue(UIState(UIStateOption.EMPTY))
+        else{
+            val floatValue = toString.toFloatOrNull()
+            if(floatValue == null)
+                uiStateManager.postValue(UIState(UIStateOption.WRONG_VALUE))
+            else{
+                uiStateManager.postValue(UIState(UIStateOption.CORRECT_VALUE))
+                currentFloatVal = floatValue
+            }
+        }
+    }
+
+
+    data class UIState (val uiState : UIStateOption)
+
+    enum class UIStateOption {
+        EMPTY, WRONG_VALUE, CORRECT_VALUE
+    }
+
     enum class Currency {
         USD,EUR,BS,BTC,ETH,PTR
     }
