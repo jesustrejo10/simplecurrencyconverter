c788933fe9435509af68c1c606be1adf82183358
Jesus <jesustrejo10@gmail.com>
2020-07-19 14:55:51 -0400

    Creating base UI to convert the currencies

--- converter/src/main/java/com/example/converter/ConversorFragment.kt
+++ converter/src/main/java/com/example/converter/ConversorFragment.kt
@@ -1,19 +1,17 @@
 package com.example.converter
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.conversor_fragment.*
 
 
-class ConversorFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = ConversorFragment()
-    }
+class ConversorFragment : Fragment(), AdapterView.OnItemClickListener,
+    AdapterView.OnItemSelectedListener {
 
     private lateinit var viewModel: ConversorViewModel
 
@@ -27,7 +25,24 @@ class ConversorFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ConversorViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        manageViewComponents()
+
+    }
+
+    private fun manageViewComponents() {
+
+        currencySpinner.onItemSelectedListener= this
+    }
+
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        viewModel.manageSelectedCurrency(position)
     }
 
 }
--- converter/src/main/java/com/example/converter/ConversorViewModel.kt
+++ converter/src/main/java/com/example/converter/ConversorViewModel.kt
@@ -3,5 +3,25 @@ package com.example.converter
 import androidx.lifecycle.ViewModel
 
 class ConversorViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+
+    fun manageSelectedCurrency(position: Int) {
+
+        println("")
+    }
+
+    private fun getCurrencyByPosition(position : Int){
+
+        when (position){
+            0 -> Currency.USD
+            1 -> Currency.EUR
+            2 -> Currency.BS
+            3 -> Currency.BTC
+            4 -> Currency.ETH
+            5 -> Currency.PTR
+        }
+    }
+    enum class Currency {
+        USD,EUR,BS,BTC,ETH,PTR
+    }
 }
