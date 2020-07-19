a7b2f34acf1f9975141b49d53ae5b19c7358013a
Jesus <jesustrejo10@gmail.com>
2020-07-19 16:56:47 -0400

    Displaying information on the screen.

--- converter/src/main/java/com/example/converter/data/model/ui/FragmentResultOperation.kt
+++ converter/src/main/java/com/example/converter/data/model/ui/FragmentResultOperation.kt
@@ -6,8 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 
 import com.example.converter.R
+import com.example.converter.data.model.ReportData
+import kotlinx.android.synthetic.main.fragment_result_operation_fragment.*
 
 class FragmentResultOperation : Fragment() {
 
@@ -26,8 +29,32 @@ class FragmentResultOperation : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(FragmentResultOperationViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this).get(FragmentResultOperationViewModel::class.java)
+        val data = viewModel.parseData(arguments?.getString("KEY_RESPONSE"))
+        if(data == null){
+            //todo display error and finish
+        }else{
+            manageData(data)
+        }
+    }
+
+    private fun manageData(data: ReportData) {
+
+        originalValueUnit.text = data.initialCurrency
+        originalValue.text = data.initialValue
+        usdValue.text = data.valueInUsd
+        usdExchangeRateValue.text = data.exchangeRateInUsd
+        eurValue.text = data.valueInEur
+        eurExchangeRateValue.text = data.exchangeRateInEur
+        bsValue.text = data.valueInBs
+        bsExchangeRateValue.text = data.exchangeRateInBs
+        btcValue.text = data.valueInBtc
+        btcExchangeRateValue.text = data.exchangeRateInBtc
+        ethValue.text = data.valueInEth
+        ethExchangeRateValue.text = data.exchangeRateInEth
+        ptrValue.text = data.valueInPtr
+        ptrExchangeRateValue.text = data.exchangeRateInPtr
+
     }
 
 }
--- converter/src/main/java/com/example/converter/data/model/ui/FragmentResultOperationViewModel.kt
+++ converter/src/main/java/com/example/converter/data/model/ui/FragmentResultOperationViewModel.kt
@@ -1,7 +1,23 @@
 package com.example.converter.data.model.ui
 
 import androidx.lifecycle.ViewModel
+import com.example.converter.data.model.ReportData
+import com.google.gson.Gson
+import java.lang.Exception
 
 class FragmentResultOperationViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    lateinit var data : ReportData
+    fun parseData(string: String?): ReportData? {
+
+        return try{
+            val gson = Gson()
+            val reportData = gson.fromJson(string,ReportData::class.java)
+            data = reportData
+            data
+        }catch (e : Exception){
+            e.printStackTrace()
+            null
+        }
+    }
 }
