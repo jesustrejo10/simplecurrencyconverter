1c4214fcaff3301af9f0cf0f708f885e24c854af
Jesus <jesustrejo10@gmail.com>
2020-07-19 20:59:26 -0400

    creating and displaying QR with the result.

--- converter/src/main/java/com/example/converter/data/model/ui/FragmentResultOperation.kt
+++ converter/src/main/java/com/example/converter/data/model/ui/FragmentResultOperation.kt
@@ -1,16 +1,18 @@
 package com.example.converter.data.model.ui
 
-import androidx.lifecycle.ViewModelProviders
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Point
 import android.os.Bundle
+import android.view.*
+import androidmads.library.qrgenearator.QRGContents
+import androidmads.library.qrgenearator.QRGEncoder
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
-
 import com.example.converter.R
 import com.example.converter.data.model.ReportData
 import kotlinx.android.synthetic.main.fragment_result_operation_fragment.*
+
 
 class FragmentResultOperation : Fragment(), View.OnClickListener {
 
@@ -66,10 +68,38 @@ class FragmentResultOperation : Fragment(), View.OnClickListener {
                 activity?.onBackPressed()
             }
             R.id.generate_qr->{
+                generateQr()
                 viewModel.generateQR()
             }
         }
 
+    }
+
+    private fun generateQr() {
+        val wm = requireContext().getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val display: Display = wm.defaultDisplay
+        val point = Point()
+        display.getSize(point)
+        val width: Int = point.x
+        val height: Int = point.y
+        var smallerDimension = if (width < height) width else height
+        smallerDimension = smallerDimension * 3 / 4
+        val inputValue = viewModel.generateQRData()
+
+        try {
+
+            val qrgEncoder = QRGEncoder(
+                inputValue, null,
+                QRGContents.Type.TEXT,
+                smallerDimension
+            )
+            qrgEncoder.colorBlack = Color.BLACK
+            qrgEncoder.colorWhite = Color.WHITE
+            val bitmap = qrgEncoder.bitmap
+            qrImage.setImageBitmap(bitmap)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
 }
--- converter/src/main/java/com/example/converter/data/model/ui/FragmentResultOperationViewModel.kt
+++ converter/src/main/java/com/example/converter/data/model/ui/FragmentResultOperationViewModel.kt
@@ -1,9 +1,13 @@
 package com.example.converter.data.model.ui
 
+import android.R.attr.bitmap
+import android.graphics.Color
+import androidmads.library.qrgenearator.QRGContents
+import androidmads.library.qrgenearator.QRGEncoder
 import androidx.lifecycle.ViewModel
 import com.example.converter.data.model.ReportData
 import com.google.gson.Gson
-import java.lang.Exception
+
 
 class FragmentResultOperationViewModel : ViewModel() {
 
@@ -24,5 +28,18 @@ class FragmentResultOperationViewModel : ViewModel() {
     fun generateQR() {
 
 
+    }
+
+    fun generateQRData(): String? {
+        val data = "Thank you for use simple currency converter." +
+                "The entered value is: ${data.initialCurrency} ${data.initialValue}" +
+                "Equivalent value in USD is ${data.valueInUsd} in base of ${data.exchangeRateInUsd}" +
+                "Equivalent value in USD is ${data.valueInEur} in base of ${data.exchangeRateInEur}" +
+                "Equivalent value in USD is ${data.valueInBs} in base of ${data.exchangeRateInBs}" +
+                "Equivalent value in USD is ${data.valueInBtc} in base of ${data.exchangeRateInBtc}" +
+                "Equivalent value in USD is ${data.valueInEth} in base of ${data.exchangeRateInEth}" +
+                "Equivalent value in USD is ${data.valueInPtr} in base of ${data.exchangeRateInPtr}"
+
+                return data
     }
 }
